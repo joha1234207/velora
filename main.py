69223d2dabc0a04123228b5a655826e6f6e52837
joha1234207 <jahonoke110099@gmail.com>
2026-05-14 02:30:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, request
 import telebot
+from flask import Flask, request
+
 from config import BOT_TOKEN, WEBHOOK_URL
 
+# Bot
 bot = telebot.TeleBot(BOT_TOKEN)
+
+# Flask
 app = Flask(__name__)
 
 # Handlers
@@ -15,25 +19,32 @@ register_message_handlers(bot)
 from callback_handler import register_callback_handlers
 register_callback_handlers(bot)
 
-# Webhook setup
-bot.remove_webhook()
-bot.set_webhook(url=f"{WEBHOOK_URL}/{BOT_TOKEN}")
+
+# Home
+@app.route("/", methods=["GET"])
+def home():
+    return "Bot ishlayapti"
 
 
-@app.route(f"/{BOT_TOKEN}", methods=["POST"])
+# Webhook
+@app.route("/webhook", methods=["POST"])
 def webhook():
-    json_str = request.get_data().decode("utf-8")
+    json_str = request.get_data().decode("UTF-8")
     update = telebot.types.Update.de_json(json_str)
+
     bot.process_new_updates([update])
+
     return "ok", 200
 
 
-@app.route("/")
-def home():
-    return "<h1>Bot is running</h1>"
-
-
+# Start
 if __name__ == "__main__":
+    bot.remove_webhook()
+
+    bot.set_webhook(
+        url=f"{WEBHOOK_URL}/webhook"
+    )
+
     app.run(
         host="0.0.0.0",
         port=10000
