ddeb65b24a5297260ac48b7c72e40e0dd11c40f4
joha1234207 <jahonoke110099@gmail.com>
2026-05-14 02:58:54 +0500

    Move command and message handler registrations

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ from commands_handler import register_command_handlers
 from message_handler import register_message_handlers
 from callback_handler import register_callback_handlers
 
-register_command_handlers(bot)
-register_message_handlers(bot)
-register_callback_handlers(bot)
 
 
 # ========================
@@ -27,6 +24,9 @@ def webhook():
         update = telebot.types.Update.de_json(json_str)
         bot.process_new_updates([update])
     return "OK", 200
+register_command_handlers(bot)
+register_message_handlers(bot)
+register_callback_handlers(bot)
 
 
 # ========================
